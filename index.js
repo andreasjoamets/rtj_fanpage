defe3f23dac05e888b201dbc167a01f1c6cb911e
Andreas Joamets <andreas.joamets@gmail.com>
2020-12-01 21:00:31 +0200

    Peaaegu valmis, kujundus veel

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-
-
-
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
+/* Kui ma vajutan nuppu, ta kas näitab või ei näita seda menüüd */
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
-// Close the dropdown if the user clicks outside of it
+// Kui ma kuhugi lehe peale vajutan, siis ta paneb menüü kinni
 window.onclick = function(e) {
   if (!e.target.matches('.dropbtn')) {
   var myDropdown = document.getElementById("myDropdown");
-    if (myDropdown.classList.contains('show')) {
-      myDropdown.classList.remove('show');
+    if (!myDropdown.classList.contains('show')) {
+      myDropdown.classList.add('show');
     }
   }
 }
